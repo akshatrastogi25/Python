5db359c878fd787be46a69e0454e8b5e0542de23
akshatrastogi25 <42384464+akshatrastogi25@users.noreply.github.com>
2020-08-17 10:12:47 +0530

    Add files via upload

---
+++ Popstack.py
@@ -0,0 +1,26 @@
+
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def add(self, dataval):
+        if dataval not in self.stack:
+            self.stack.append(dataval)
+            return True
+        else:
+            return False
+        
+    def remove(self):
+        if len(self.stack) <= 0:
+            return ("No element in the Stack")
+        else:
+            return self.stack.pop()
+
+AStack = Stack()
+AStack.add("Mon")
+AStack.add("Tue")
+AStack.add("Wed")
+AStack.add("Thu")
+print(AStack.remove())
+print(AStack.remove())
