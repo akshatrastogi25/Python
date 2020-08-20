2930a643242c4574803853abc18704f60aed1fd4
akshatrastogi25 <42384464+akshatrastogi25@users.noreply.github.com>
2020-08-20 14:22:05 +0530

    Update Mergesort.py

--- Mergesort.py
+++ Mergesort.py
@@ -1,7 +1,7 @@
 def merge_sort(unsorted_list):
     if len(unsorted_list) <= 1:
         return unsorted_list
-# Find the middle point and devide it
+
     middle = len(unsorted_list) // 2
     left_list = unsorted_list[:middle]
     right_list = unsorted_list[middle:]
@@ -10,7 +10,7 @@ def merge_sort(unsorted_list):
     right_list = merge_sort(right_list)
     return list(merge(left_list, right_list))
 
-# Merge the sorted halves
+
 
 def merge(left_half,right_half):
 
