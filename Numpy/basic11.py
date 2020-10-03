c940264a13aee517212cdae836b6ee795a8ff979
Akshat Rastogi <42384464+akshatrastogi25@users.noreply.github.com>
2020-10-04 00:06:02 +0530

    Add files via upload

---
+++ Numpy/basic11.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.random.randn(10, 4),
+   index = pd.date_range('1/1/2000', periods=10),
+   columns = ['A', 'B', 'C', 'D'])
+print df.rolling(window=3).mean()
