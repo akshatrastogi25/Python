140ebf38731d8660c402b575ccc6d9a1a726324a
Akshat Rastogi <42384464+akshatrastogi25@users.noreply.github.com>
2021-02-22 17:28:36 +0530

    Add files via upload

--- BaisicImplementation/P01_CountNumbers.py
+++ BaisicImplementation/P01_CountNumbers.py
@@ -1,6 +1,6 @@
 # Your task is pretty simple , given a string S , find the total count of numbers present in the digit.
 #
-# Input
+# Input 
 #
 # The first line contains T , the number of test cases. The first line of each and every testc ase will contain a integer N ,
 # the length of the string . The second line of each and every test case will contain a string S of length N.
