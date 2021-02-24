e1f3f8e6b4b76e446e6c1a526a74c12d4df71704
Akshat Rastogi <42384464+akshatrastogi25@users.noreply.github.com>
2021-02-24 17:39:25 +0530

    Add files via upload

--- Selectionsort.py
+++ Selectionsort.py
@@ -1,6 +1,6 @@
 def selection_sort(input_list):
 
-    for idx in range(len(input_list)):
+    for idx in range(len(input_list)): 
 
         min_idx = idx
         for j in range( idx +1, len(input_list)):
